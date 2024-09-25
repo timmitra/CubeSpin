84ac7b9e3b5ca25ee6fc2bf33c706448023d08ee
Tim Mitra <tim@it-guy.com>
2024-09-25 15:35:13 -0400

    Initial Commit

---
+++ CubeSpin/CubeSpinApp.swift
@@ -0,0 +1,21 @@
+//
+// ---------------------------- //
+// Original Project: CubeSpin
+// Created on 2024-09-25 by Tim Mitra
+// Mastodon: @timmitra@mastodon.social
+// Twitter/X: timmitra@twitter.com
+// Web site: https://www.it-guy.com
+// ---------------------------- //
+// Copyright © 2024 iT Guy Technologies. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct CubeSpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
