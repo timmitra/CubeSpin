3b7db63df7c6a3c69dddef01cbf5ba6bf393f9ae
Tim Mitra <tim@it-guy.com>
2024-09-25 15:50:01 -0400

    add rotation and ornament to show rotation

--- CubeSpin/CubeModel3DView.swift
+++ CubeSpin/CubeModel3DView.swift
@@ -13,9 +13,30 @@ import SwiftUI
 import RealityKit
 
 struct CubeModel3DView: View {
+    @State private var rotationY: Double = 0.0
+    
     var body: some View {
-        Model3D(named: "Cube")
-            .padding3D(.back, 80)
+        Model3D(named: "Cube") { model in
+                model
+                .rotation3DEffect(
+                    .degrees(rotationY),
+                    axis: (x: 0, y: 1, z: 0)
+                )
+        } placeholder: {
+            ProgressView()
+        }.gesture(
+            DragGesture(minimumDistance: 0)
+                .onChanged { value in
+                    rotationY += Double(value.translation.width / 100)
+                    rotationY = rotationY.truncatingRemainder(dividingBy: 360)
+                }
+        )
+        .ornament(attachmentAnchor: .scene(.bottom)) {
+            Text("Rotation: \(rotationY, specifier: "%.2f")º")
+                .padding()
+                .glassBackgroundEffect()
+        }
+        .padding3D(.back, 80)
     }
 }
 
